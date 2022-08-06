9f9a776c6b96a09aebf837c66101402bea649eff
İlhan <lhan@DESKTOP-QKC4CCP>
2022-08-06 23:34:54 +0300

    FluentValidation Eklendi.

---
+++ BusinessLayer/ValidationRules/CategoryValidatior.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class CategoryValidatior:AbstractValidator<Category>
+    {
+        public CategoryValidatior()
+        {
+            RuleFor(p => p.CategoryName).NotEmpty().WithMessage("Kategori adını boş geçemezsiniz.");
+            RuleFor(p => p.CategoryDescription).NotEmpty().WithMessage("Açıklamayı boş geçemezsiniz.");
+            RuleFor(p => p.CategoryName).MinimumLength(3).WithMessage("Kategori adı 3 karakterden az olamaz.");
+            RuleFor(p => p.CategoryName).MaximumLength(20).WithMessage("Lütfen 20 karakterden fazla değer girişi yapmayın.");
+        }
+
+    }
+}
